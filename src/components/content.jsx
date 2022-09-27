e894c43a71f89fc91c64e96dc70a212a0a1cecbd
AhmadRaza <ahmadraza.azt@gmail.com>
2022-09-27 16:53:42 +0500

    initial

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,5 +1,7 @@
 // import Posts from './post';
-import { BsStars } from "react-icons/bs";
+// import { BsStars } from "react-icons/bs";
+// import { FaRegImage } from "react-icons/bs";
+
 
 
 
@@ -10,6 +12,14 @@ let Content = () => {
             <div className='content'>
                 <div className="home">
                     <h2>Home</h2>
+                </div>
+                <div className="home2">
+                    <img src="https://avatars.githubusercontent.com/u/86877851?s=400&u=29c92d79043f506ee35a390b12953fd998b5402a&v=4" className="profile_photo" alt="profile_photo" />
+                   <input type="text" placeholder=" What's Happening ?"  className="input_profile" />
+                    {/* <h2>Ahmed Raza</h2> */}
+                    <div className="icon_profile">
+                        {/* <FaRegImage /> */}
+                    </div>
                 </div>
             
             </div>
