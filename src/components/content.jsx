34d2e7417a9558c88160eb432725269a5f39cc25
AhmadRaza <ahmadraza.azt@gmail.com>
2022-09-27 17:50:27 +0500

    intial

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,6 +1,10 @@
 // import Posts from './post';
 // import { BsStars } from "react-icons/bs";
-// import { FaRegImage } from "react-icons/bs";
+import { FaRegImage } from "react-icons/fa";
+import { CgMenuBoxed } from "react-icons/cg";
+import { GrEmoji } from "react-icons/gr";
+import { BsCalendarCheckFill } from "react-icons/bs";
+
 
 
 
@@ -17,8 +21,16 @@ let Content = () => {
                     <img src="https://avatars.githubusercontent.com/u/86877851?s=400&u=29c92d79043f506ee35a390b12953fd998b5402a&v=4" className="profile_photo" alt="profile_photo" />
                    <input type="text" placeholder=" What's Happening ?"  className="input_profile" />
                     {/* <h2>Ahmed Raza</h2> */}
-                    <div className="icon_profile">
-                        {/* <FaRegImage /> */}
+                </div>
+                <div className="icon_profile">
+                    <div className="cover">
+                        <FaRegImage  className="cover_icons"/>
+                        <CgMenuBoxed  className="cover_icons"/>
+                        <GrEmoji  className="cover_icons"/>
+                        <BsCalendarCheckFill  className="cover_icons"/>
+                    </div>    
+                    <div className="btn_profile">
+                        <button className="button_profile">Profile</button>
                     </div>
                 </div>
             
