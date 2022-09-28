b3d35f3ceac7cdaae377a05d5b4fa3bbd6033b0a
AhmadRaza <ahmadraza.azt@gmail.com>
2022-09-29 04:35:30 +0500

    add para

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -7,7 +7,8 @@ import { render } from "@testing-library/react";
 let Posts = () =>{
     return(
         <div className="post">
-            <h1>محمد</h1>
+            <h1>محمد</h1> 
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta pariatur, nemo dolore distinctio omnis veniam dolorum provident voluptas, sint, aut rem sit facere ab officia non maiores ea. Error, molestiae!</p>
         </div>
 
     );
